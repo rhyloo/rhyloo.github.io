bd743959d0a98c97c76c1b90c06ecc9155e76b97
Rhyloo <jorge@uma.es>
2021-02-10 16:54:16 +0100

    Some fixes

--- script/test.js
+++ script/test.js
@@ -1,24 +1,30 @@
+let testBool = false;
+// var data = sessionStorage.getItem('key');
+    // console.log('Default value of bool is',testBool); 
 function updateBackground() {
-  var 
+    var
     hr = (new Date()).getHours(),
     body = document.body,
     bstyle = body.style,
     x = document.body;
     y = x.getElementsByClassName("book-menu");
-    hello = document.querySelector(".hello");    
-  if (hr < 16) {
-    bstyle.backgroundColor = "white";
+    hello = document.querySelector(".hello");
+    testBool = !testBool;
+    // sessionStorage.setItem('key', testBool);
+    // console.log('Toggled bool is',testBool); 
+    if (hr<16) {
+    bstyle.backgroundColor = "gray";
     bstyle.color = "black";
       for (i = 0; i < y.length; i++) {
 	  y[i].style.backgroundColor = "black";
-	  y[i].style.color = "red";
+	  y[i].style.color = "yellow";
       }
-           
   } else {
     bstyle.backgroundColor = "black";
-    bstyle.color = "white";
+      bstyle.color = "white";
   } 
 }
 
-setInterval(updateBackground, 1000 * 60);
+// setInterval(updateBackground, 1000 * 60);
 updateBackground();
+// https://www.w3schools.com/html/html5_webstorage.asp
