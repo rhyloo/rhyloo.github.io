ebab321e45a02706835a605fb5d56506e363972f
Rhyloo <jorge@uma.es>
2021-02-11 17:25:35 +0100

    some fixes

---
+++ script/mode.js
@@ -0,0 +1,32 @@
+let testBool = false;
+// var data = sessionStorage.getItem('key');
+    // console.log('Default value of bool is',testBool); 
+function updateBackground() {
+    var
+    hr = (new Date()).getHours(),
+    hr = 1,
+    body = document.body,
+    bstyle = body.style,
+    x = document.body;
+    y = x.getElementsByClassName("book-menu");
+    hello = document.querySelector(".hello");
+    testBool = !testBool;
+    // sessionStorage.setItem('key', testBool);
+    // console.log('Toggled bool is',testBool); 
+    if (hr<16) {
+	// bstyle.backgroundColor = "#616771";
+	bstyle.backgroundColor = "#cccccc";
+    bstyle.color = "black";
+      for (i = 0; i < y.length; i++) {
+	  y[i].style.backgroundColor = "white";
+	  y[i].style.color = "#cccccc";
+      }
+  } else {
+    bstyle.backgroundColor = "#4267B3";
+      bstyle.color = "red";
+  } 
+}
+
+// setInterval(updateBackground, 1000 * 60);
+updateBackground();
+// https://www.w3schools.com/html/html5_webstorage.asp
